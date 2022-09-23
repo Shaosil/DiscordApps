fbf7742d7c71164413148f7025fb86cba70902a2
Shaosil <bbeck1988@gmail.com>
2022-09-22 23:49:45 -0400

    Confirmed working - reverting fail test

--- ShaosilBot.Tests/SlashCommands/TestCommandTests.cs
+++ ShaosilBot.Tests/SlashCommands/TestCommandTests.cs
@@ -6,11 +6,5 @@ namespace ShaosilBot.Tests.SlashCommands
     public class TestCommandTests : SlashCommandTestBase<TestCommand>
     {
         protected override TestCommand GetInstance() => new TestCommand(CommandLoggerMock.Object);
-
-		[TestMethod]
-		public void FailTest()
-		{
-			Assert.Fail("Test fail message!");
-		}
     }
 }
