8696383b377890e4cba017c1e172d331d720bd35
Shaosil <bbeck1988@gmail.com>
2022-09-22 20:13:02 -0400

    Triggering action

--- ShaosilBot.Tests/SlashCommands/TestCommandTests.cs
+++ ShaosilBot.Tests/SlashCommands/TestCommandTests.cs
@@ -6,7 +6,5 @@ namespace ShaosilBot.Tests.SlashCommands
     public class TestCommandTests : SlashCommandTestBase<TestCommand>
     {
         protected override TestCommand GetInstance() => new TestCommand(CommandLoggerMock.Object);
-
-		// Todo
     }
 }
